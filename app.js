2d9024b8b224225064eadf11bd10e66f872ed8b4
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2019-11-25 14:50:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const promises = [];
+for (let i = 1; i <= 150; i++) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+  promises.push(fetch(url).then(res => res.json()));
+}
+
+Promise.all(promises).then(results => {
+  //console.log(results);
+  const pokemon = results.map(data => ({
+    name: data.name,
+    id: data.id,
+    image: data.sprites["front_default"],
+    type: data.types.map(type => type.type.name).join(", ")
+  }));
+  displayPokemon(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      pokeman =>
+        `
+      <li class="card">
+          <img class="card-image" src="${pokeman.image}"/>
+          <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+          <p class="card-subtitle">Type: ${pokeman.type}</p>
+      </li>
+      `
+    )
+    .join("");
+  const pokedex = document.getElementById("pokedex");
+
+  pokedex.innerHTML = pokemonHTMLString;
+});
+
+const displayPokemon = pokemon => {
+  //console.log(pokemon);
+  const pokemonHTMLString = pokemon
+    .map(
+      pokeman =>
+        `
+    <li class="card">
+        <img class="card-image" src="${pokeman.image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+        <p class="card-subtitle">Type: ${pokeman.type}</p>
+    </li>
+    `
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
+};
